a4544898844ccccb307cbd3690b1ea90453dcb62
jprincip21 <jprincip21@gmail.com>
2026-03-30 22:29:30 -0400

    Created Schedule Model to be used in scheduleRepository

---
+++ models/schedule.py
@@ -0,0 +1,29 @@
+# SFWRTECH 4SA3 - Software Architecture
+# CalPal Project - Schedule Model
+# Jonathan Principato (400527847)
+
+# Represents an Schedule entity in the CalPal system.
+# Maps to the Schedule table in the CalPal MySQL database.
+# Used by ScheduleRepository to structure data returned from the database.
+
+class Schedule:
+    def __init__(self,
+                 id,
+                 location_id,
+                 start_date,
+                 end_date,
+                 state):
+        self.__id = id
+        self.__location_id = location_id
+        self.__start_date = start_date
+        self.__end_date = end_date
+        self.__state = state
+
+    def get_schedule_info(self):
+        return {
+            "id": self.__id,
+            "location_id": self.__location_id,
+            "start_date": str(self.__start_date), # Strings instead of date objects
+            "end_date": str(self.__end_date),
+            "state": self.__state
+        }
