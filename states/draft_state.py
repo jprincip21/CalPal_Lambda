39ac67b5761fb9456f55b895592511e5b87f603c
jprincip21 <jprincip21@gmail.com>
2026-04-03 19:42:01 -0400

    Added email sending logic to publish

--- states/draft_state.py
+++ states/draft_state.py
@@ -4,7 +4,6 @@
 
 from states.schedule_state import ScheduleState
 
-
 # Design Pattern: State
 # Is the initial state of every schedule
 # Drafts can only transition to published
@@ -13,12 +12,14 @@ class DraftState(ScheduleState):
     def __init__(self, schedule_id, schedule_repo, shift_repo):
         super().__init__(schedule_id, schedule_repo, shift_repo)
 
-    def publish(self):
+    def publish(self, notification_service):
         shifts = self._shift_repo.get_shifts_by_schedule_id(self._schedule_id)
         if not shifts:
             raise ValueError("Cannot Publish schedule with no shifts.")
         
         self._schedule_repo.update_state(self._schedule_id, "published")
+        notification_service.send_schedule_notification(self._schedule_id)
+
 
     def complete(self):
         raise ValueError("Cannot change state from draft to complete.")
